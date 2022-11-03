e70d26a675021c3ea87c943dd06c33a57454cac1
fuyemura <fuyemura@gmail.com>
2022-11-02 21:52:17 -0300

    Versão final

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-CoffeeMenu = Menu()
-CoffeeResource = CoffeeMaker()
-CoffeeMoney = MoneyMachine()
+coffee_menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
 
 is_machine_on = True
 while is_machine_on:
-    CoffeeDrink = input(f'What would you like? {CoffeeMenu.get_items()}: ').lower()
+    coffee_drink = input(f'What would you like? {coffee_menu.get_items()}: ').lower()
 
-    if CoffeeDrink not in ['espresso', 'latte', 'cappuccino', 'report', 'off']:
+    if coffee_drink not in ['espresso', 'latte', 'cappuccino', 'report', 'off']:
         print('invalid option')
-    elif CoffeeDrink == 'report':
-        CoffeeResource.report()
-        CoffeeMoney.report()
-    elif CoffeeDrink == 'off':
+    elif coffee_drink == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    elif coffee_drink == 'off':
         is_machine_on = False
     else:
-        Drink = CoffeeMenu.find_drink(CoffeeDrink)
+        Drink = coffee_menu.find_drink(coffee_drink)
 
-        if CoffeeResource.is_resource_sufficient(Drink):
-            if CoffeeMoney.make_payment(Drink.cost):
-                CoffeeResource.make_coffee(Drink)
+        if coffee_maker.is_resource_sufficient(Drink):
+            if money_machine.make_payment(Drink.cost):
+                coffee_maker.make_coffee(Drink)
